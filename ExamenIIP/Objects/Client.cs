1a0feaaacfdec2fce40ad166c133bbea97c80efc
TheFoo @WIN <jf_mejiar547@live.com>
2018-06-26 11:32:18 -0600

    Objects

---
+++ ExamenIIP/Objects/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenIIP.Objects
+{
+    public class Client
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string cedula { get; set; }
+        public string cel { get; set; }
+        public string email { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+
+        public Client(int id, string name, string surname, string cedula, string cel, string email, string address,string city, string state){
+            this.id = id;
+            this.cedula = cedula;
+            this.name = name;
+            this.surname = surname;
+            this.cel = cel;
+            this.email = email;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+        }
+
+        public string[] ToArray()
+        {
+            string[] arr = {this.id.ToString(),this.cedula, this.name, this.surname, this.cel, this.email, this.address,
+            this.city, this.state};
+            return arr;
+        }
+    }
+}
---
+++ ExamenIIP/Objects/Extinguisher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenIIP.Objects
+{
+    public class Extinguisher
+    {
+        public int id { get; set; }
+        public char cat { get; set; }
+        public string brand { get; set; }
+        public TYPE type { get; set; }
+        public int cap { get; set; }
+        public string und { get; set; }
+        public string place { get; set; }
+        public DateTime date { get; set; }
+        public Client owner { get; set; }
+        public enum TYPE{Agua,Químico,CO2}
+        public Extinguisher(int id, char cat, string brand, int type, int cap, string und, string place, DateTime date, Client owner)
+        {
+            this.id = id;
+            this.cat = cat;
+            this.brand = brand;
+            this.type = (TYPE)type;
+            this.cat = cat;
+            this.und = und;
+            this.place = place;
+            this.date = date;
+            this.owner = owner;
+        }
+    }
+}
